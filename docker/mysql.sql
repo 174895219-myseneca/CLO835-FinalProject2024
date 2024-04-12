514e8669914b812055c1ee5cb5d259d5660440b6
Augustine Antwi <aantwi6@myseneca.ca>
2024-04-12 16:45:10 -0400

    requirements updated

--- docker/mysql.sql
+++ docker/mysql.sql
@@ -6,9 +6,11 @@ emp_id VARCHAR(20),
 first_name VARCHAR(20),
 last_name VARCHAR(20),
 primary_skill VARCHAR(20),
-location VARCHAR(20));
+location VARCHAR(20),
+PRIMARY KEY (emp_id)
+);
 
 INSERT INTO employee VALUES ('1','Amanda','Williams','Smile','local');
-INSERT INTO employee VALUES ('1','Alan','Williams','Empathy','alien');
-SELECT * FROM employee;
+INSERT INTO employee VALUES ('2','Alan','Williams','Empathy','alien');
 
+SELECT * FROM employee;
